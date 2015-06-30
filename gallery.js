1b3317f9d64ad91b14d0de27457ed5fe5caa4271
Sergei Panfilov <flakessp@gmail.com>
2015-06-30 20:17:15 +0300

    Unfinished form

--- gallery.js
+++ gallery.js
@@ -28,3 +28,50 @@ $('a.next').click(function (e) {
     e.preventDefault();
     moveLeft();
 });
+   
+    //select all the a tag with name equal to modal
+    $('a.button-order').click(function(e) {
+        console.log("loaded!")
+        //Cancel the link behavior
+        e.preventDefault();
+        
+        var id = $("#dialog");
+    
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+    
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').appendTo('body');
+
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+        
+        //transition effect        
+        // $('#mask').fadeIn(1000);    
+        $('#mask').fadeTo("fast",0.8);    
+    
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+              
+        //Set the popup window to center
+        $(id).css('top',  winH/2-$(id).height()/2);
+        $(id).css('left', winW/2-$(id).width()/2);
+    
+        //transition effect
+        $(id).fadeIn(2000); 
+        
+    });
+    
+
+    //if mask is clicked
+    $('#mask').click(function () {
+        $(this).hide();
+        $('.window').hide();
+        player.destroy();
+    });  
+    $('#dialogClose').click(function () {
+        $("#mask").hide();
+        $('.window').hide();
+        player.destroy();
+    });
